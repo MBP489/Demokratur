3643b9570b13e779db37a1f765f3ed63330e967e
MarkB <mark.broderius@yahoo.de>
2019-03-11 19:41:49 +0100

    developed program

--- demokratur.cpp
+++ demokratur.cpp
@@ -3,35 +3,36 @@
 
 using namespace std;
 
-void initField();
-int* move(int stepsX, int stepsY, int *position);
+int* initField(unsigned int fieldsize, int parties);
+int* moveRel(int stepsX, int stepsY, int *position);
+int* moveAbs(int stepsX, int stepsY, int *position);
 int* left(int* position);
 int* right(int* position);
 int* up(int* position);
 int* down(int* position);
 int* getRandNeighbour(int* candidate);
 
-const int fieldX = 20;
-const int fieldY = 20;
-const int parties = 2;
-const int steps = 40000;
-
-const int fieldsize = fieldX * fieldY;
-int *const field = new int[fieldsize];
-
 random_device *const rd = new random_device();
 mt19937 *const engine = new mt19937(rd);
 
-uniform_int_distribution<int> *const randCandidate = new uniform_int_distribution<int>(0, fieldsize-1);
 uniform_int_distribution<int> *const randNeighbour = new uniform_int_distribution<int>(0, 3);
 
 int main() {
 
-    initField();
+    const unsigned int fieldX = 20;
+    const unsigned int fieldY = 20;
+    const unsigned int parties = 2;
+    const unsigned int steps = 40000;
 
-    int* candidate = field + (*randCandidate)(engine);
-    int* neighbour = getRandNeighbour(candidate);
+    const unsigned int fieldsize = fieldX * fieldY;
+    int *const field = initField(fieldsize, parties);
+    int *position = field;
+    uniform_int_distribution<int> *const randCandidate = new uniform_int_distribution<int>(0, fieldsize-1);
 
+    for (unsigned int i = 0; i < steps; i++) {
+        int* candidate = field + (*randCandidate)(engine);
+        int* neighbour = getRandNeighbour(candidate);
+    }
 }
 
 int* getRandNeighbour(int* candidate){
@@ -56,29 +57,35 @@ int* getRandNeighbour(int* candidate){
 }
 
 int* left(int *position) {
-    return move(-1, 0, position);
+    return moveRel(-1, 0, position);
 }
 
 int* right(int *position) {
-    return move(1, 0, position);
+    return moveRel(1, 0, position);
 }
 
 int* up(int *position) {
-    return move(0, -1, position);
+    return moveRel(0, -1, position);
 }
 
 int* down(int *position) {
-    return move(0, 1, position);
+    return moveRel(0, 1, position);
 }
 
-int* move(int stepsX, int stepsY, int *position) {
+int* moveRel(int stepsX, int stepsY, int *position) {
 
 }
 
-void initField() {
+int* moveAbs(int stepsX, int stepsY, int *position) {
+
+}
+
+int* initField(unsigned int fieldsize, int parties) {
     uniform_int_distribution<int> randInit(0, parties - 1);
-    for (int i = 0; i < fieldsize; i++) {
+    int *field = new int[fieldsize];
+    for (unsigned int i = 0; i < fieldsize; i++) {
         int *p = field + i;
         *p = randInit(engine);
     }
+    return field;
 }
