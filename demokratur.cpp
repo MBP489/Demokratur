b896aa79bba865a5154554745a0a539dce5e41d0
broderius_mar@K2AC.LOCAL <broderius_mar@K2AC.LOCAL>
2019-03-12 12:15:08 +0100

    update

--- demokratur.cpp
+++ demokratur.cpp
@@ -3,34 +3,50 @@
 
 using namespace std;
 
-int* initField(unsigned int fieldsize, int parties);
-int* move(int *const field, int fieldsize, int *position, int steps);
-int* left(int *const field, int fieldX, int fieldY, int *position);
-int* right(int *const field, int fieldX, int fieldY, int *position);
-int* up(int *const field, int fieldX, int fieldY, int *position);
-int* down(int *const field, int fieldX, int fieldY, int *position);
+void init(int X, int Y, int part, int st);
 int* getRandNeighbour(int* candidate);
+int* pos(int x, int y);
+int x(int *position);
+int y(int *position);
+int* left(int *position);
+int* right(int *position);
+int* up(int *position);
+int* down(int *position);
+int* initField(int fieldsize, int parties);
 
 random_device *const rd = new random_device();
 mt19937 *const engine = new mt19937(rd);
-
 uniform_int_distribution<int> *const randNeighbour = new uniform_int_distribution<int>(0, 3);
+uniform_int_distribution<int> *randField;
+
+int parties;
+int steps;
+
+int *field;
+int fieldX;
+int fieldY;
+int fieldsize;
+
+void init(int X, int Y, int part, int st) {
+    fieldX = X;
+    fieldY = Y;
+    fieldsize = fieldX * fieldY;
+    parties = part;
+    steps = st;
+
+    field = initField(fieldsize, parties);
+
+    randField = new uniform_int_distribution<int>(0, fieldsize - 1);
+}
 
 int main() {
+    init(20, 20, 2, 40000);
+    int *candidate;
+    int *neighbour;
 
-    const int fieldX = 20;
-    const int fieldY = 20;
-    const int parties = 2;
-    const int steps = 40000;
-
-    const int fieldsize = fieldX * fieldY;
-    int *const field = initField(fieldsize, parties);
-    int *position = field;
-    uniform_int_distribution<int> *const randCandidate = new uniform_int_distribution<int>(0, fieldsize-1);
-
-    for (unsigned int i = 0; i < steps; i++) {
-        int* candidate = field + (*randCandidate)(engine);
-        int* neighbour = getRandNeighbour(candidate);
+    for (int i = 0; i < steps; i++) {
+        candidate = field[(*randField)(engine)];
+        neighbour = getRandNeighbour(candidate);
     }
 }
 
@@ -39,47 +55,69 @@ int* getRandNeighbour(int* candidate){
     int choosen = (*randNeighbour)(engine);
     switch (choosen) {
     case 0: {
-
+        return left(position);
     }
     case 1: {
-
+        return right(position);
     }
     case 2: {
-
+        return up(position);
     }
     case 3: {
-
+        return down(position);
     }
     default:
         return nullptr;
     }
 }
 
-int* move(const int *field, int fieldsize, int *position, int steps) {
+int* pos(int x, int y) {
+    int *position = field + x * fieldX + y;
     if(field <= position && position <= (field + fieldsize)) {
-        int* newPosition = position + steps;
-        if(field <= newPosition && newPosition <= (field + fieldsize)) {
-            position = newPosition;
-            return position;
-        }
+        return position;
     }
     return nullptr;
 }
 
-int* left(int *const field, int fieldX, int fieldY, int *position) {
-
+int x(int *position) {
+    int count = position - field;
+    if(count < 0 || count > fieldsize) {
+        return nullptr;
+    }
+    return count / fieldX;
 }
 
-int* right(int *const field, int fieldX, int fieldY, int *position) {
-
+int y(int *position) {
+    int count = position - field;
+    if(count < 0 || count > fieldsize) {
+        return nullptr;
+    }
+    int y = count % fieldX;
 }
 
-int* up(int *const field, int fieldX, int fieldY, int *position) {
-
+int* left(int *position) {
+    int x = x(position);
+    position = (x - 1 >= 0) ? position-- : position + fieldX - 1;
+    return position;
 }
 
-int* down(int *const field, int fieldX, int fieldY, int *position) {
 
+int* right(int *position) {
+    int x = x(position);
+    position = (x + 1 < fieldX) ? position++ : position - fieldX + 1;
+    return position;
+}
+
+int* up(int *position) {
+    int y = y(position);
+    position = (y - fieldX >= 0) ? position - fieldX : position - fieldX + fieldsize;
+    return position;
+}
+
+int* down(int *position) {
+    int y = y(position);
+    position = (y + fieldX < fieldY) ? position + fieldX : position + fieldX - fieldsize;
+    return position;
 }
 
 int* initField(int fieldsize, int parties) {
