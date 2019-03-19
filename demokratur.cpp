98e201641bdfc63555b5f1e2772c75897a41994c
broderius_mar@K2AC.LOCAL <broderius_mar@K2AC.LOCAL>
2019-03-19 12:17:26 +0100

    program is functioning

--- demokratur.cpp
+++ demokratur.cpp
@@ -15,65 +15,65 @@ int* right(int *position);
 int* up(int *position);
 int* down(int *position);
 int* initField(int fieldsize, int parties);
+int getRand(int inclusiveUpperBound);
 void printField();
-
-static random_device rd;
-static mt19937 engine(rd());
-static uniform_int_distribution<int> randNeighbour(0, 3);
-static uniform_int_distribution<int> randField;
 
 static int *parties;
 static int *field;
 static int fieldX;
 static int fieldY;
 static int steps;
+static int counter;
 
-void init(int X, int Y, int* part, int st) {
+void init(int X, int Y, int *part, int partsize, int st) {
     fieldX = X;
     fieldY = Y;
-    parties = part;
     steps = st;
+    counter = 0;
 
-    field = initField(fieldX * fieldY, sizeof(part)/sizeof(*part));
+    parties = new int[partsize];
+    for(int i = 0; i < partsize; i++){
+        int *p = parties + i;
+        *p = part[i];
+    }
 
-    uniform_int_distribution<int> temp(0, fieldX * fieldY - 1);
-    randField = temp;
+    field = initField(fieldX * fieldY, partsize);
 }
 
 int main() {
     int parties[] = {50, 50};
-    init(20, 20, parties, 40000);
+    init(20, 20, parties, 2, 1000000);
+    printField();
     int *candidate;
     int *neighbour;
-    int counter = 0;
+    int limit = 0;
+    int fieldsize = fieldX * fieldY;
     for (int i = 0; i < steps; i++) {
-        candidate = field + randField(engine);
+        candidate = field + getRand(fieldsize);
         neighbour = getRandNeighbour(candidate);
         if(*candidate != *neighbour) {
             talk(candidate, neighbour);
         }
+        limit++;
         counter++;
-        if(counter > 1000){
-            system("cls");
+        if(limit > 10000){
             printField();
-            Sleep(500);
-            counter = 0;
+            limit = 0;
         }
     }
 }
 
 void talk(int *candidate, int *neighbour) {
-    int* partyCandidate = parties + *candidate;
-    int* partyNeighbour = parties + *neighbour;
-    uniform_int_distribution<int> randParty(1, *partyCandidate + *partyNeighbour);
-    if(randParty(engine) > *partyCandidate) {
+    int partyCandidate = parties[*candidate];
+    int partyNeighbour = parties[*neighbour];
+    if(getRand(partyCandidate + partyNeighbour) >= partyCandidate) {
         *candidate = *neighbour;
     }
 }
 
 int* getRandNeighbour(int* candidate){
     int* position = candidate;
-    int choosen = randNeighbour(engine);
+    int choosen = getRand(4);
     switch (choosen) {
     case 0: {
         return left(position);
@@ -119,7 +119,7 @@ int getY(int *position) {
 int* left(int *position) {
     int x = getX(position);
     if(x > -1) {
-        position = (x - 1 >= 0) ? position-- : position + fieldX - 1;
+        position = (x - 1 >= 0) ? --position : position + fieldX - 1;
         return position;
     }
     return nullptr;
@@ -128,7 +128,7 @@ int* left(int *position) {
 int* right(int *position) {
     int x = getX(position);
     if(x > -1) {
-        position = (x + 1 < fieldX) ? position++ : position - fieldX + 1;
+        position = (x + 1 < fieldX) ? ++position : position - fieldX + 1;
         return position;
     }
     return nullptr;
@@ -137,7 +137,7 @@ int* right(int *position) {
 int* up(int *position) {
     int y = getY(position);
     if(y > -1) {
-        position = (y - fieldX >= 0) ? position - fieldX : position - fieldX + fieldX * fieldY;
+        position = (position - fieldX >= field) ? position - fieldX : position - fieldX + fieldX * fieldY;
         return position;
     }
     return nullptr;
@@ -146,7 +146,7 @@ int* up(int *position) {
 int* down(int *position) {
     int y = getY(position);
     if(y > -1) {
-        position = (y + fieldX < fieldY) ? position + fieldX : position + fieldX - fieldX * fieldY;
+        position = (position + fieldX < field + fieldX * fieldY) ? position + fieldX : position + fieldX - fieldX * fieldY;
         return position;
     }
     return nullptr;
@@ -154,11 +154,10 @@ int* down(int *position) {
 
 int* initField(int fieldsize, int parties) {
     if(fieldsize > 0 && parties > 0) {
-        uniform_int_distribution<int> randInit(0, parties - 1);
         int *field = new int[unsigned(fieldsize)];
         for (int i = 0; i < fieldsize; i++) {
             int *p = field + i;
-            *p = randInit(engine);
+            *p = getRand(parties);
         }
         return field;
     }
@@ -166,11 +165,18 @@ int* initField(int fieldsize, int parties) {
 }
 
 void printField() {
+    system("cls");
     int *pos = field;
     for (int y = 0; y < fieldY; y++) {
         for (int x = 0; x < fieldX; x++) {
-            cout << *pos++;
+            cout << *pos;
+            pos++;
         }
         cout << endl;
     }
+    cout << "steps: " << counter << endl;
+}
+
+int getRand(int exclusiveUpperBound) {
+    return rand() % exclusiveUpperBound;
 }
