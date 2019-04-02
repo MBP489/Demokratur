f071f18fd755935259c2af4821705550c5e0ec7b
broderius_mar@k2ac.local <broderius_mar@k2ac.local>
2019-04-02 11:43:05 +0200

    update

--- demokratur.cpp
+++ demokratur.cpp
@@ -8,6 +8,7 @@ using namespace std;
 void init(int X, int Y, int part, int st);
 int* getRandNeighbour(int* candidate);
 void talk(int *candidate, int *neighbour);
+void swap(int *pos1, int *pos2);
 int* pos(int x, int y);
 int  getX(int *position);
 int  getY(int *position);
@@ -15,25 +16,29 @@ int* left(int *position);
 int* right(int *position);
 int* up(int *position);
 int* down(int *position);
+int* initFieldRand(int fieldsize, int parties);
 int* initField(int fieldsize, int parties);
 int* initParties(int *parties, int partiessize);
 int getRand(int inclusiveUpperBound);
 bool isUniform();
-void printField();
+void printField(int time);
 
 static mt19937 engine;
 
+static char signs[4] = {'.', 'X', '%', '#'};
 static int *parties;
 static int *field;
 static int fieldX;
 static int fieldY;
 static int steps;
+static int printSteps;
 static int counter;
 
-void init(int X, int Y, int *part, int partsize, int st) {
+void init(int X, int Y, int *part, int partsize, int st, int prtSteps) {
     fieldX = X;
     fieldY = Y;
     steps = st;
+    printSteps = prtSteps;
     counter = 0;
 
     random_device rd;
@@ -46,36 +51,45 @@ void init(int X, int Y, int *part, int partsize, int st) {
 
 int main() {
     int parties[] = {50, 50};
-    init(20, 20, parties, 2, 70000000);
-    printField();
+    init(20, 20, parties, 2, 70000000, 100);
     int *candidate;
     int *neighbour;
     int limit = 0;
     int fieldsize = fieldX * fieldY;
+
+    printField(0);
     while (true) {
         candidate = field + getRand(fieldsize);
         neighbour = getRandNeighbour(candidate);
-        if(*candidate != *neighbour) {
-            talk(candidate, neighbour);
-        }
+        talk(candidate, neighbour);
+//        swap(candidate, neighbour);
         counter++;
         if(counter > steps || isUniform()) {
             break;
         }
         limit++;
-        if(limit > 10000){
-            printField();
+        if(limit > printSteps){
+            printField(0);
             limit = 0;
         }
     }
-    printField();
+    printField(0);
+}
+
+void swap(int *pos1, int *pos2) {
+    int temp = *pos1;
+    *pos1 = *pos2;
+    *pos2 = temp;
 }
 
 void talk(int *candidate, int *neighbour) {
-    int partyCandidate = parties[*candidate];
-    int partyNeighbour = parties[*neighbour];
-    if(getRand(partyCandidate + partyNeighbour) >= partyCandidate) {
-        *candidate = *neighbour;
+    if(*candidate != *neighbour) {
+        int partyCandidate = parties[*candidate];
+        int partyNeighbour = parties[*neighbour];
+        int rand = getRand(partyCandidate + partyNeighbour);
+        if(rand >= partyCandidate) {
+            *candidate = *neighbour;
+        }
     }
 }
 
@@ -171,6 +185,26 @@ int* initParties(int *parties, int partiessize) {
 
 int* initField(int fieldsize, int parties) {
     int *field = new int[unsigned(fieldsize)];
+    int size = fieldsize / parties;
+    int oversize = fieldsize & parties;
+    for(int i = 0; i < parties; i++) {
+        if(i < (parties - 1)) {
+            for(int j = i * size; j < (i + 1) * size; j++) {
+                int *p = field + j;
+                *p = i;
+            }
+        } else {
+            for(int j = (parties - 1) * size; j < parties * size + oversize; j++) {
+                int *p = field + j;
+                *p = i;
+            }
+        }
+    }
+    return field;
+}
+
+int* initFieldRand(int fieldsize, int parties) {
+    int *field = new int[unsigned(fieldsize)];
     for (int i = 0; i < fieldsize; i++) {
         int *p = field + i;
         *p = getRand(parties);
@@ -178,17 +212,19 @@ int* initField(int fieldsize, int parties) {
     return field;
 }
 
-void printField() {
+void printField(int time) {
     system("cls");
+    cout << "DEMOKRATUR" << endl << endl;
     int *pos = field;
     for (int y = 0; y < fieldY; y++) {
         for (int x = 0; x < fieldX; x++) {
-            cout << *pos;
+            cout << signs[*pos];
             pos++;
         }
         cout << endl;
     }
-    cout << "COUNTER: " << counter << endl;
+    cout << endl << "COUNTER: " << counter << endl;
+    Sleep(time);
 }
 
 bool isUniform() {
